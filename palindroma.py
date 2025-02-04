a67626a95c6ff2d5fb165f15ebd90db21ab02b84
camilo andres <camiloandrescaicedousuga87@gmail.com>
2025-02-04 18:59:37 -0500

    vercion refactorizada

--- palindroma.py
+++ palindroma.py
@@ -1,15 +1,9 @@
 word = input("write a word: ")
 
-top = len(word)
-right = 0
-lef = top - 1
-
-while right <= lef:
-    if word[right] == word[lef]:
-        right += 1
-        lef -= 1
-    else:
+for a, b in zip(word, reversed(word)):
+    if a != b:
         print("no es palindroma")
         break
 else:
     print("es palindroma")
+
